a0666ea6bd795b352081ec2e77fc32a954661143
t0tem <vadimkul@gmail.com>
2017-10-20 12:14:43 +0200

    cost and gradient done.

--- lrCostFunction.m
+++ lrCostFunction.m
@@ -36,14 +36,9 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
-
-
-
-
-
-
-
-
+h = sigmoid(X*theta);
+J = -1/m * (y'*log(h) + (1-y)'*log(1-h)) + lambda/(2*m) * sum(theta(2:end,:).^2);
+grad = 1/m * X' * (h - y) + lambda/m * [0;theta(2:end,:)];
 
 % =============================================================
 
